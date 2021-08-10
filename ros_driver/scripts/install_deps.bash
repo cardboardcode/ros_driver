7563558144fafbbbfcb789afe0c90a87ccbff32e
Bey Hao Yun <beyhy94@gmail.com>
2021-08-11 00:56:05 +0800

    :hammer: Verified prematurely install_deps.bash script.

--- ros_driver/scripts/install_deps.bash
+++ ros_driver/scripts/install_deps.bash
@@ -6,7 +6,13 @@
 # Written by Bey Hao Yun <beyhy94@gmail.com>, August 2021
 
 git submodule update --init --recursive
-cd deps/pcl/
-mkdir build && cd build
+cd deps/pcl/ || exit
+
+if [ ! -d 'build']; then
+  mkdir build
+fi
+
+cd build || exit
+echo $PWD
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/../local ..
 make install -j$(nproc)
